e7d52e7d36321b5d7e2dc951928845a70b4766d8
ElmahdiK <elmahdi.korfed@gmail.com>
2023-07-31 20:49:06 +0200

    fix : object destructuring for animals sounds

--- assets/portfolio/services/animalssounds/assets/js/js.js
+++ assets/portfolio/services/animalssounds/assets/js/js.js
@@ -6,18 +6,18 @@
 const $ = document.querySelector.bind(document);
 const $$ = document.querySelectorAll.bind(document);
 import d from "./data.js";
+let _audio = $(`audio`);
 
-window.onload = _ => d.forEach((e, index) => $('#ul_animals').insertAdjacentHTML('beforeEnd', templateCardAnimal(e, index)));
+window.onload = _ => d.map((e, index) => $('#ul_animals').insertAdjacentHTML('beforeEnd', templateCardAnimal(e, index)));
 
 window.playAudio = animal => {
-    let _audio = $(`audio`);
     _audio.src = `https://www.google.com/logos/fnbx/animal_sounds/${animal}.mp3`;
     _audio.play();
 }
 
-const templateCardAnimal = (_element, _num) => `<li id="li_${_num}" data-name="${_element.name}" onclick="playAudio('${_element.name}')">
+const templateCardAnimal = ({ name, img }) => `<li onclick="playAudio('${name}')">
 <div class="square">
-<img src="${_element.img}" alt="${_element.name}" onerror="this.onerror=null; this.src='./assets/images/notfound.jpg'">
-<p>${_element.name}</p>
+<img src="${img}" alt="${name}" onerror="this.onerror=null; this.src='./assets/images/notfound.jpg'">
+<p>${name}</p>
 </div>
 </li>`;
