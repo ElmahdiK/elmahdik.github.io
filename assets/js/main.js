d70ad85e37f0e70c272f8519d0e089842c579f82
ElmahdiK <elmahdi.korfed@gmail.com>
2024-11-02 22:20:35 +0100

    feat(index): ajout de la photo de profil

---
+++ assets/js/main.js
@@ -0,0 +1,35 @@
+(function () {
+    emailjs.init({
+        publicKey: "Xt5kvG1KQ0QbFg03l",
+    });
+})();
+
+window.onload = () => {
+    const bsTheme = localStorage.getItem("bsTheme");
+    const bsThemeCurrent = document.body.dataset.bsTheme;
+    if (bsTheme && (bsThemeCurrent != bsTheme)) myFunction();
+
+    let contactForm = document.getElementById('contact-form');
+    contactForm.addEventListener('submit', function (event) {
+        event.preventDefault();
+        emailjs.sendForm('default_service', 'template_ptculdh', this)
+            .then(() => {
+                console.log('SUCCESS!');
+                contactForm.reset();
+                alert("Thank you, your message has been sent !")
+            }, (error) => {
+                console.log('FAILED...', error);
+            });
+    });
+
+    // particlesJS.load('particles-js', '../assets/js/particles.json', function () {
+    //     console.log('callback - particles-js config loaded');
+    // });
+}
+const myFunction = () => {
+    let element = document.body;
+    element.dataset.bsTheme =
+        element.dataset.bsTheme == "light" ? "dark" : "light";
+    document.querySelector("[name=icon--darkMode]").className = element.dataset.bsTheme == "light" ? "bi bi-moon-stars fs-5" : "bi bi-brightness-high fs-5";
+    localStorage.setItem("bsTheme", element.dataset.bsTheme);
+}
